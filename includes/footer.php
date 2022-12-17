931811fb57f563b75601575ef13b15f63d634182
Navdeep Singh <Navdeepsingh4298@gmail.com>
2022-12-17 22:37:25 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 
 <footer class="text-center container-fluid bg-dark text-warning p-2">
 	<p>&copy; <a href="index.php" class="link">Nav Real Estates</a> <?php echo date("Y"); ?> <br>
-	Made in India By <a href="https://www.navdev.ml" class="link" target="_blank" rel="noopener">Navdeep Singh</a></p>
+ 	Made in India By <!--<a href="https://www.navdev.ml" class="link" target="_blank" rel="noopener">Navdeep Singh</a> -->
+	</p>
 </footer>
 </body>
 </html>
